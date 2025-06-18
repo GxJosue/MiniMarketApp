c43765b0c91f0332bcba4a7b5317e59ee5cfedc2
Josué Martínez <josuegt0001@gmail.com>
2025-06-18 06:17:49 -0600

    MiniMarketApp: Fix issues

--- app/src/main/java/com/example/minimarketapp/MainActivity.java
+++ app/src/main/java/com/example/minimarketapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // vamos a crear este layout
+        setContentView(R.layout.activity_main);
 
         tvSaludo = findViewById(R.id.tvSaludo);
         btnProductos = findViewById(R.id.btnProductos);
